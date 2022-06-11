e17fa7e4867774f5d0b7c01558573bc63568ec44
Utkarshraj007 <59512749+Utkarshraj007@users.noreply.github.com>
2022-06-11 15:55:54 +0530

    first commit

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey = "2b759ae9";
---
+++ src/components/MovieListing/MovieListing.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useSelector } from 'react-redux';
+import { getAllShows,getAllMovies } from '../../features/movies/movieSlice';
+import MovieCard from '../MovieCard/MovieCard'
+import "./MovieListing.scss"
+const MovieListing = () => {
+  const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
+  let renderMovies = "";
+  let renderShows = "";
+  renderMovies = movies.Response === "True" ? (movies.Search.map((movie,index)=>{
+    return <MovieCard key= {index} data={movie}/>;
+  })
+  ):(<div className='movies-error'><h3>{movies.error}</h3></div>);
+  console.log(movies);
+
+  renderShows =
+  shows.Response === "True" ? (
+    shows.Search.map((movie, index) => <MovieCard key={index} data={movie} />)
+  ) : (
+    <div className="shows-error">
+      <h3>{shows.Error}</h3>
+    </div>
+  );
+  return (
+    <div className="movie-wrapper">
+      <div className="movie-list">
+        <h2>Movies</h2>
+        <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieListing
